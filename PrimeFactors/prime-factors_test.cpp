7b6f92971a6da394cdd7339796046946e4685abe
haewon <baehw1107@naver.com>
2025-07-02 10:14:08 +0900

    [feature] 41

--- PrimeFactors/prime-factors_test.cpp
+++ PrimeFactors/prime-factors_test.cpp
@@ -1,9 +1,11 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, PrimeTest) {
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
+//[feature] page 번호
+// [refactoring] page 번호
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
