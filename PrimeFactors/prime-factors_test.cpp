c6317b58c00f84c3fc42d21498aaaaedccfe4c28
haewon <baehw1107@naver.com>
2025-07-02 10:19:18 +0900

    [refactoring] 46

--- PrimeFactors/prime-factors_test.cpp
+++ PrimeFactors/prime-factors_test.cpp
@@ -4,14 +4,19 @@
 //[feature] page 번호
 // [refactoring] page 번호
 
-TEST(PrimeFactors, Of1) {
+using namespace testing;
+class PrimeFixture :public Test {
+public:
 	PrimeFactors prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactors prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
