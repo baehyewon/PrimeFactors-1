c6a6432310d47da8ecbdd53747e03a6a990364f5
haewon <baehw1107@naver.com>
2025-07-02 10:35:24 +0900

    [refactoring] 55

--- PrimeFactors/prime-factors.cpp
+++ PrimeFactors/prime-factors.cpp
@@ -2,12 +2,14 @@
 using std::vector;
 class PrimeFactors {
 public:
-	vector<int> of(int number){
+	vector<int> of(int number) {
 		vector<int> result = {};
 		if (number > 1) {
 			if (number == 4) {
-				result.push_back(2);
-				result.push_back(2);
+				while (number % 2 == 0) {
+					result.push_back(2);
+					number /= 2;
+				}
 			}
 			else {
 				result.push_back(number);
